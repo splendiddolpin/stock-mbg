29005ba61f1be10e016700368afe2a60da0d3147
splendiddolpin <112374251+splendiddolpin@users.noreply.github.com>
2026-05-21 13:15:34 +0700

    Add files via upload

---
+++ app/Models/Item.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $fillable = [
+        'name', 'stock_system', 'hpp', 'min_stock_warning', 'unit'
+    ];
+
+    // Logika Status Kata
+    public function getStatusAttribute()
+    {
+        if ($this->stock_system <= 0) return 'Habis';
+        if ($this->stock_system <= $this->min_stock_warning) return 'Hampir Habis';
+        return 'Aman';
+    }
+
+    // Logika Warna Tailwind (Badge)
+    public function getStatusColorAttribute()
+    {
+        return match($this->status) {
+            'Habis' => 'bg-red-100 text-red-800 border-red-300',
+            'Hampir Habis' => 'bg-yellow-100 text-yellow-800 border-yellow-300',
+            default => 'bg-green-100 text-green-800 border-green-300',
+        };
+    }
+    public function transactions() {
+    return $this->hasMany(Transaction::class);
+    }
+}
